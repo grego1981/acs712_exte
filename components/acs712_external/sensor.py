6d3760f0e3eeff3f483ccb1eef837936cceed472
grego1981 <aladou.web@gmail.com>
2025-02-24 11:36:11 +0200

    Update sensor.py

--- components/acs712_external/sensor.py
+++ components/acs712_external/sensor.py
@@ -1,7 +1,11 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor
-from esphome.const import UNIT_AMPERE, ICON_CURRENT_AC
+from esphome.const import (
+    UNIT_AMPERE,
+    ICON_CURRENT_AC,
+    CONF_ID,
+)
 
 DEPENDENCIES = []
 
@@ -12,11 +16,9 @@ CONFIG_SCHEMA = sensor.sensor_schema(
     unit_of_measurement=UNIT_AMPERE,
     icon=ICON_CURRENT_AC,
     accuracy_decimals=2,
-).extend(cv.polling_component_schema()).extend({
-    cv.Required("name"): cv.string,  # Name of the sensor in ESPHome
-})
+).extend(cv.polling_component_schema())
 
 async def to_code(config):
-    var = cg.new_Pvariable(config["name"])
+    var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await sensor.register_sensor(var, config)
