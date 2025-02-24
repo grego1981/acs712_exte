fb87d0df121503e4cff3cfe1dfd30dd070f53be5
grego1981 <aladou.web@gmail.com>
2025-02-24 13:48:12 +0200

    Add files via upload

--- components/acs712_external/sensor.py
+++ components/acs712_external/sensor.py
@@ -9,16 +9,14 @@ from esphome.const import (
 
 DEPENDENCIES = []
 
-acs712_ns = cg.esphome_ns.namespace("acs712_external")
-ACS712Sensor = acs712_ns.class_("ACS712Sensor", cg.PollingComponent, sensor.Sensor)
+acs712_ns = cg.esphome_ns.namespace('acs712_external')
+ACS712Sensor = acs712_ns.class_('ACS712Sensor', cg.PollingComponent, sensor.Sensor)
 
 CONFIG_SCHEMA = sensor.sensor_schema(
-    unit_of_measurement=UNIT_AMPERE,
-    icon=ICON_CURRENT_AC,
-    accuracy_decimals=2,
-).extend(cv.polling_component_schema())
+    ACS712Sensor, unit_of_measurement=UNIT_AMPERE, icon=ICON_CURRENT_AC, accuracy_decimals=2).extend(cv.polling_component_schema('60s'))
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await sensor.register_sensor(var, config)
+    cg.add_library("RobTillaart/ACS712", "1.0.0")
