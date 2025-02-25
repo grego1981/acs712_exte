6273f3a241b5814478af6cb91fc1672e7ef52308
grego1981 <aladou.web@gmail.com>
2025-02-25 08:40:08 +0200

    Update acs712_component.h

--- components/acs712_external/acs712_component.h
+++ components/acs712_external/acs712_component.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#include "esphome/core/component.h"  // Core ESPHome features
-#include "esphome/components/sensor/sensor.h"  // Include Sensor
+#include "esphome/core/component.h"  // ESPHome core components
+#include "esphome/components/sensor/sensor.h"  // Sensor class
 #include "ACS712.h"
 
 namespace esphome {
@@ -9,13 +9,16 @@ namespace acs712_external {
 
 class ACS712Sensor : public PollingComponent {
    public:
-    ACS712Sensor() : PollingComponent(15000) {}
+    ACS712Sensor() : PollingComponent(15000) {
+        current_sensor = new sensor::Sensor();  // Allocate properly
+        power_sensor = new sensor::Sensor();
+    }
 
     void setup() override;
     void update() override;
 
-    sensor::Sensor *current_sensor = new sensor::Sensor();
-    sensor::Sensor *power_sensor = new sensor::Sensor();
+    sensor::Sensor *current_sensor;  // Declare pointers properly
+    sensor::Sensor *power_sensor;
 
    private:
     ACS712 *ACS = new ACS712(A0, 5, 1023, 100);
