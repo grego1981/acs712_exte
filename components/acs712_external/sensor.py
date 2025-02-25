babf864909a039cf33e5bf9e512c4d4ed206e874
grego1981 <aladou.web@gmail.com>
2025-02-25 08:41:02 +0200

    Update sensor.py

--- components/acs712_external/sensor.py
+++ components/acs712_external/sensor.py
@@ -1,22 +1,35 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor
-from esphome.const import (
-    UNIT_AMPERE,
-    ICON_CURRENT_AC,
-    CONF_ID,
-)
+from esphome.const import UNIT_AMPERE, UNIT_WATT, ICON_FLASH, ICON_CURRENT_AC
 
 DEPENDENCIES = []
 
-acs712_ns = cg.esphome_ns.namespace('acs712_external')
-ACS712Sensor = acs712_ns.class_('ACS712Sensor', cg.PollingComponent, sensor.Sensor)
+acs712_ns = cg.esphome_ns.namespace("acs712_external")
+ACS712Sensor = acs712_ns.class_("ACS712Sensor", cg.PollingComponent)
 
-CONFIG_SCHEMA = sensor.sensor_schema(
-    ACS712Sensor, unit_of_measurement=UNIT_AMPERE, icon=ICON_CURRENT_AC, accuracy_decimals=2).extend(cv.polling_component_schema('60s'))
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(ACS712Sensor),
+    cv.Optional("current"): sensor.sensor_schema(
+        unit_of_measurement=UNIT_AMPERE,
+        icon=ICON_CURRENT_AC,
+        accuracy_decimals=2,
+    ),
+    cv.Optional("power"): sensor.sensor_schema(
+        unit_of_measurement=UNIT_WATT,
+        icon=ICON_FLASH,
+        accuracy_decimals=2,
+    ),
+})
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
+    var = cg.new_Pvariable(config[cv.GenerateID()])
     await cg.register_component(var, config)
-    await sensor.register_sensor(var, config)
-    cg.add_library("RobTillaart/ACS712", "0.3.10")
+
+    if "current" in config:
+        sens = await sensor.new_sensor(config["current"])
+        cg.add(var.current_sensor, sens)
+
+    if "power" in config:
+        sens = await sensor.new_sensor(config["power"])
+        cg.add(var.power_sensor, sens)
